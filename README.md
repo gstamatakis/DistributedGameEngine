d65b28178ca3d6da90193c9b85a6c063570375f0
Giorgos Stamatakis <giorgoshstam@gmail.com>
2020-04-13 20:28:31 +0300

    Init

---
+++ README.md
@@ -0,0 +1,7 @@
+Authentication
+
+    https://blog.ngopal.com.np/2017/10/10/spring-boot-with-jwt-authentication-using-redis/
+
+
+
+
---
+++ spring/src/main/java/authentication/domain/SessionUser.java
@@ -0,0 +1,51 @@
+
+package authentication.domain;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+@Data
+public class SessionUser {
+
+    private String username;
+
+    private String password;
+
+    private Date created;
+
+    public boolean hasExpired() {
+        if(created == null){
+            return true;
+        }
+        LocalDateTime localDateTime = created.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+        localDateTime = localDateTime.plusHours(1);
+        return  Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant()).before(new Date());
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+}
